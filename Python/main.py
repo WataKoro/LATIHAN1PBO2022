6a9718ff74d55954e114ddd7502ed49762a1a184
IrfanEsa <72029919+WataKoro@users.noreply.github.com>
2022-02-14 19:43:06 +0700

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,17 +1,21 @@
+#import dua class
 from DataTim import DataTim
 from DataTim import DataPemain
 
+#define dan mengisi atribut dari class data tim
 tim = DataTim()
 tim.setNama("Madrid")
 tim.setAsal("Spanyol")
 tim.setTahun(1908)
 
+#print isi atribut class data tim
 print('===============================')
 print("Nama  : "+str(tim.getNama()))
 print("Asal  : "+str(tim.getAsal()))
 print("Tahun : "+str(tim.getTahun()))
 print('===============================')
 
+#define dan ini atribut dari class data pemain
 pemain1 = DataPemain()
 pemain1.setNama("Marcelo")
 pemain1.setNomor(12)
@@ -26,6 +30,7 @@ pemain2.setPosisi("Forward")
 pemain2.setAsal("Perancis")
 pemain2.setTahun(1987)
 
+#print isi atribut data pemain
 print('===============================')
 print("Nama   : "+str(pemain1.getNama()))
 print("Nomor  : "+str(pemain1.getNomor()))
