74ff3440d1722ff5a150ceda5ea6c631045dbfbb
IrfanEsa <72029919+WataKoro@users.noreply.github.com>
2022-02-14 19:33:09 +0700

    Update DataTim.php

--- PHP/DataTim.php
+++ PHP/DataTim.php
@@ -1,16 +1,20 @@
 <?php
 
+//class data tim
 class DataTim{
+    //atribut dari class data tim
     private $nama = 0;
     private $asal = 0;
     private $tahun = 0;
-
+    
+    //constuctor
     public function __construct($nama = 0, $asal = 0, $tahun = 0){
         $this->nama = $nama;
         $this->asal = $asal;
         $this->tahun = $tahun;
     }
-
+    
+    //fungsi untuk menyimpan nilai dan memanggil atribut
     public function setNama($nama){
         $this->nama = $nama;
     }
