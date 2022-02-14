ac777dc0b0f5cdaa6b5d552eaacde56201a51f81
IrfanEsa <72029919+WataKoro@users.noreply.github.com>
2022-02-14 19:31:50 +0700

    Update DataPemain.php

--- PHP/DataPemain.php
+++ PHP/DataPemain.php
@@ -1,14 +1,17 @@
 <?php
-
+//class data pemain inheritance dengan data tim
 class DataPemain extends DataTim{
+    //berisi nomor dan posisi, sisanya sudah inherit dari data tim
     private $nomor = 0;
     private $posisi = 0;
-
+    
+    //constructor
     public function __construct($nomor = 0, $posisi = 0){
         $this->nomor = $nomor;
         $this->posisi = $posisi;
     }
-
+    
+    //fungsi untuk menyimpan dan memanggil atribut dalam class
     public function setNomor($nomor){
         $this->nomor = $nomor;
     }
